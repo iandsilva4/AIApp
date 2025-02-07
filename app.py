34191eea9ac9d89e9c7e6cf29852c5ee922a31a6
iandsilva4 <iandsilva4@gmail.com>
2025-02-06 23:42:50 -0600

    new port

--- app.py
+++ app.py
@@ -77,6 +77,6 @@ def get_entries():
         for e in entries
     ])
 
-# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no port is provided
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
